7ff6943cc6e58e1d3c336ead8c8182b64697afd1
Guillaume VARA <guillaume.vara@gmail.com>
2019-02-16 12:07:33 +0100

    preparing profile interface

--- front/ui/header.php
+++ front/ui/header.php
@@ -1,13 +1,16 @@
 <header>
-    <div class='fctr' id='logo'>
-        <span>WTNZ</span>
-    </div>
-    <div id='banner'>
-        <div id='banner-desc'>
-            <?php echo i18n('libraryOfHead', $user_qs)?>
+    <div id='menu'>
+        <div class='fctr' id='logo'>
+            <span>WTNZ</span>
         </div>
-        <?php include 'front/ui/menu.php' ?>
-        <?php include 'front/ui/searchBand.php' ?>
-        <div class='anim'></div>
+        <div id='banner'>
+            <div id='banner-desc'>
+                <?php echo i18n('libraryOfHead', $user_qs)?>
+            </div>
+            <?php include 'front/ui/menu.php' ?>
+            <?php include 'front/ui/searchBand.php' ?>
+        </div>
     </div>
+    <!--<div style="width:100%; border-top: 1px solid rgba(0,0,0,.1);transition: .2s all; min-height:0"></div>-->
+    <div class='anim'></div>
 </header>
