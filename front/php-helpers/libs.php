e57ca2be2e6810283c4d3f549e35feb1157d65d1
Amphaal <Amphaal@MacBook-Pro-de-Utilisateur.local>
2019-02-18 02:50:54 +0100

    fix async

--- front/php-helpers/libs.php
+++ front/php-helpers/libs.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/solid.css" async>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/brands.css" async>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/fontawesome.css" async>
-<script type="text/javascript" src="front/js/ext/highcharts.js" async></script>
-<script type="text/javascript" src="front/js/ext/moment-with-locales.min.js" async></script>
-<script type="text/javascript" src="front/js/ext/sorttable.js" async></script>
-<script type="text/javascript" src="front/js/ext/socket.io.js" async></script>
+<script type="text/javascript" src="front/js/ext/highcharts.js"></script>
+<script type="text/javascript" src="front/js/ext/moment-with-locales.min.js"></script>
+<script type="text/javascript" src="front/js/ext/sorttable.js"></script>
+<script type="text/javascript" src="front/js/ext/socket.io.js"></script>
