a20f8656c0a01054c39688482ab2dc6a01eeeaa2
Guillaume VARA <guillaume.vara@gmail.com>
2019-03-01 17:36:49 +0100

    fix download file

--- back/controllers/downloadApp.php
+++ back/controllers/downloadApp.php
@@ -5,7 +5,11 @@ function routerDownload($action) {
 
     $latestInFolder = scandir($initialPath, SCANDIR_SORT_DESCENDING)[0];
     $filepath = $initialPath . "/" . $latestInFolder;
-
+    
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary"); 
     header('Content-Disposition: attachment; filename="'.$latestInFolder.'"');
+    
+    ob_clean(); flush();
     readfile($filepath);
 }
