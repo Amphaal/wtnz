03bdb64ab01c2bf9e8d2b9f18ef02e95e03ef200
Amphaal <guillaume.vara@gmail.com>
2023-09-07 05:46:17 +0200

    wip

--- src/const.php
+++ src/const.php
@@ -1,6 +1,13 @@
 <?php 
 
+/** this app name */
 $appName = "SoundVitrine";
+
+/** the companion app name */
 $companionAppName = "SoundBuddy";
+
+/** */
 $expectedUploadedLibraryFilename = $appName . "_file";
+
+/** */
 $expectedShoutFilename = "shout_file";
--- src/translations/fr.php
+++ src/translations/fr.php
@@ -1,6 +1,6 @@
 <?php 
 
-include $_SERVER['DOCUMENT_ROOT'] . "/config/const.php";
+include $_SERVER['DOCUMENT_ROOT'] . "/const.php";
 
 return array(
     'worksBest' => 'Fonctionne mieux avec',
