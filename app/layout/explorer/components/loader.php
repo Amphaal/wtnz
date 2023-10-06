17a3691200449849fd4026a11e15492b4b9fb7a1
Amphaal <guillaume.vara@gmail.com>
2023-10-06 07:52:18 +0200

    fix

--- app/layout/explorer/components/loader.php
+++ app/layout/explorer/components/loader.php
@@ -2,7 +2,7 @@
     <div id='loader'>
             <div id='useChrome' class='loading-text'>
                 <img src='/public/images/chrome.svg' alt="Chrome Logo"/>
-                <div><?= i18n('worksBest')?></div>
+                <div style="text-align: center"><?= i18n('worksBest')?></div>
             </div>
         <div id='loader-bar'></div>
     </div>
