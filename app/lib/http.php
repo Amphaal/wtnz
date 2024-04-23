38ed6cf9e754fe98f996c0f49438699aae73f2b9
Amphaal <guillaume.vara@gmail.com>
2024-04-23 13:23:55 +0200

    fix URL with params

--- app/lib/http.php
+++ app/lib/http.php
@@ -1,9 +1,31 @@
 <?php
 
+/**
+ * may return empty array on "/" or "/?..."
+ */
 function getQueryString($request_uri = null) {
     if(!$request_uri) $request_uri = $_SERVER['REQUEST_URI'];
-    $request_uri = explode('/', $request_uri);
-    $request_uri = array_filter($request_uri, 'strlen' );
+    $request_uri = explode('/', $request_uri); // split components on slashes
+    $request_uri = array_filter($request_uri, 'strlen' ); // remove non-empty
+    $request_uri = array_values($request_uri); // reapply indexes (if any removes)
+
+    //
+    if(!empty($request_uri)) {
+        //
+        // lets check if last URL component contains "?" params splitter, and if so, ignores params into returning array
+        //
+
+        // get last component
+        $request_maybe_params_raw = $request_uri[array_key_last($request_uri)];
+
+        // explode it so we remove params from URL
+        $last_url_component = explode('?', $request_maybe_params_raw)[0]; // grab index 0; only get URL part, not params !
+        
+        // replace last component without params in-array
+        $request_uri[array_key_last($request_uri)] = $last_url_component;
+    }
+
+    //
     return $request_uri;
 }
 
